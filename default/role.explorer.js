9457206a64200918f0654e4d6f03ac4479b81e45
Mathias Worthington-Bourg <mathias.wb20@gmail.com>
2025-04-14 20:55:35 +0100

    updated explorer logic to actually function properly

--- default/role.explorer.js
+++ default/role.explorer.js
@@ -11,7 +11,27 @@ module.exports = {
         // If no mode is set, set to exploring
         if (!creep.memory.mode || creep.memory.mode === config.MODE.idle) {
             creep.memory.mode = config.MODE.exploring;
-            creep.memory.currentGoal = "findIntersection"; // Goals: findIntersection, followLine, findDeposit, harvestDeposit, returnHome
+            creep.memory.currentGoal = "findHighway"; // Goals: findHighway, followHighway, harvestDeposit, returnHome
+            creep.memory.homeRoom = creep.room.name;
+        }
+        
+        // If creep is full, switch to storing mode
+        if (creep.store.getFreeCapacity() === 0 && creep.memory.mode === config.MODE.exploring) {
+            creep.memory.mode = config.MODE.storing;
+            return;
+        }
+        
+        // If creep is empty and storing, return to exploring or suicide
+        if (creep.store.getUsedCapacity() === 0 && creep.memory.mode === config.MODE.storing) {
+            // If we had already found a deposit, suicide after delivering
+            if (creep.memory.foundDeposit) {
+                creep.suicide();
+                return;
+            }
+            
+            // Otherwise, go back to exploring
+            creep.memory.mode = config.MODE.exploring;
+            return;
         }
         
         switch (creep.memory.mode) {
@@ -31,68 +51,89 @@ module.exports = {
     exploreAndHarvest: function(creep) {
         creepHelper.say(creep);
         
-        // Switch to storing mode if full
-        if (creep.store.getFreeCapacity() === 0) {
-            creep.memory.mode = config.MODE.storing;
-            creep.memory.currentGoal = "returnHome";
-            creep.memory.targetRoom = config.ROOM_NAME;
-            return;
-        }
-        
-        if (creep.memory.currentGoal === "findIntersection") {
-            const currentCoords = this.parseRoomName(creep.room.name);
-            // Find closest intersection (room with coordinates divisible by 10)
-            const targetCoords = this.findClosestIntersection(currentCoords.x, currentCoords.y);
-            const targetRoom = `${targetCoords.xDir}${targetCoords.x}${targetCoords.yDir}${targetCoords.y}`;
+        if (creep.memory.currentGoal === "findHighway") {
+            const coords = this.parseRoomName(creep.room.name);
             
-            if (creep.room.name === targetRoom) {
-                creep.memory.currentGoal = "followLine";
+            // Find closest highway (where one coordinate is 0)
+            if (this.isHighwayRoom(creep.room.name)) {
+                creep.memory.currentGoal = "followHighway";
                 creep.memory.direction = Math.random() < 0.5 ? "horizontal" : "vertical";
-            } else {
-                creep.memory.targetRoom = targetRoom;
-                const exitDir = creep.room.findExitTo(creep.memory.targetRoom);
-                const exit = creep.pos.findClosestByRange(exitDir);
-                creepHelper.moveTo(creep, exit);
+                return;
             }
+            
+            // Determine direction to nearest highway
+            let targetRoom;
+            if (coords.x % 10 < coords.y % 10) {
+                // Get to a room where X is divisible by 10
+                const targetX = Math.floor(coords.x / 10) * 10;
+                targetRoom = `${coords.xDir}${targetX}${coords.yDir}${coords.y}`;
+            } else {
+                // Get to a room where Y is divisible by 10
+                const targetY = Math.floor(coords.y / 10) * 10;
+                targetRoom = `${coords.xDir}${coords.x}${coords.yDir}${targetY}`;
+            }
+            
+            this.travelToRoom(creep, targetRoom);
         }
         
-        if (creep.memory.currentGoal === "followLine") {
-            const deposits = creep.room.find(FIND_DEPOSITS);
+        else if (creep.memory.currentGoal === "followHighway") {
+            // First check for deposits in current room
+            // In actual game use FIND_DEPOSITS, for simulation use FIND_MINERALS
+            const deposits = creep.room.find(FIND_MINERALS);
+            
             if (deposits.length > 0) {
                 creep.memory.currentGoal = "harvestDeposit";
                 creep.memory.depositId = deposits[0].id;
                 return;
             }
             
+            // Continue along highway
             const coords = this.parseRoomName(creep.room.name);
             let nextRoom;
             
             if (creep.memory.direction === "horizontal") {
-                nextRoom = `${coords.xDir}${coords.x + 10}${coords.yDir}${coords.y}`;
+                // Move along X axis (East or West)
+                const nextX = coords.x + (coords.xDir === 'E' ? 1 : -1);
+                nextRoom = `${coords.xDir}${nextX}${coords.yDir}${coords.y}`;
             } else {
-                nextRoom = `${coords.xDir}${coords.x}${coords.yDir}${coords.y + 10}`;
+                // Move along Y axis (North or South)
+                const nextY = coords.y + (coords.yDir === 'N' ? 1 : -1);
+                nextRoom = `${coords.xDir}${coords.x}${coords.yDir}${nextY}`;
             }
             
-            creep.memory.targetRoom = nextRoom;
-            const exitDir = creep.room.findExitTo(creep.memory.targetRoom);
-            const exit = creep.pos.findClosestByRange(exitDir);
-            creepHelper.moveTo(creep, exit);
+            this.travelToRoom(creep, nextRoom);
         }
         
-        if (creep.memory.currentGoal === "harvestDeposit") {
+        else if (creep.memory.currentGoal === "harvestDeposit") {
             const deposit = Game.getObjectById(creep.memory.depositId);
             
-            if (deposit && deposit.cooldown === 0) {
-                if (creep.harvest(deposit) === ERR_NOT_IN_RANGE) {
-                    creepHelper.moveTo(creep, deposit);
+            // If deposit exists and has minerals left
+            if (deposit) {
+                // When using FIND_MINERALS for testing, check if there are minerals left
+                if (deposit.mineralAmount > 0) {
+                    // For real deposits, check cooldown
+                    // if (deposit.cooldown === 0) {
+                    if (creep.harvest(deposit) === ERR_NOT_IN_RANGE) {
+                        creepHelper.moveTo(creep, deposit);
+                    }
+                    creep.memory.foundDeposit = true;
+                } else {
+                    // Source is depleted, go back home
+                    creep.memory.mode = config.MODE.storing;
                 }
             } else {
-                creep.memory.currentGoal = "findIntersection";
+                // Invalid deposit ID, resume exploration
                 delete creep.memory.depositId;
+                creep.memory.currentGoal = "findHighway";
             }
         }
     },
     
+    /**
+     * Parse room name into coordinates and directions
+     * @param {string} roomName - The room name (e.g. "E15N10")
+     * @returns {Object} Parsed room coordinates
+     */
     parseRoomName: function(roomName) {
         const match = roomName.match(/([EW])(\d+)([NS])(\d+)/);
         return {
@@ -103,15 +144,45 @@ module.exports = {
         };
     },
     
-    findClosestIntersection: function(x, y) {
-        const targetX = Math.round(x / 10) * 10;
-        const targetY = Math.round(y / 10) * 10;
-        return {
-            x: targetX,
-            y: targetY,
-            xDir: x >= 0 ? 'E' : 'W',
-            yDir: y >= 0 ? 'N' : 'S'
-        };
+    /**
+     * Check if a room is on a highway (has a coordinate divisible by 10)
+     * @param {string} roomName - The room name to check
+     * @returns {boolean} True if the room is on a highway
+     */
+    isHighwayRoom: function(roomName) {
+        const coords = this.parseRoomName(roomName);
+        return coords.x % 10 === 0 || coords.y % 10 === 0;
+    },
+    
+    /**
+     * Travel to a specific room
+     * @param {Creep} creep - The creep to move
+     * @param {string} roomName - The target room name
+     */
+    travelToRoom: function(creep, roomName) {
+        // If already in target room, return true
+        if (creep.room.name === roomName) {
+            return true;
+        }
+        
+        // Find exit to target room
+        const exitDir = Game.map.findExit(creep.room, roomName);
+        if (exitDir === ERR_NO_PATH) {
+            // No path to room, try a different one
+            creep.memory.currentGoal = "findHighway";
+            return false;
+        }
+        
+        const exit = creep.pos.findClosestByPath(exitDir);
+        if (!exit) {
+            // No viable exit, try a different approach
+            creep.memory.currentGoal = "findHighway";
+            return false;
+        }
+        
+        // Move to exit
+        creepHelper.moveTo(creep, exit);
+        return false;
     },
     
     /**
@@ -121,43 +192,36 @@ module.exports = {
     storeResources: function(creep) {
         creepHelper.say(creep);
         
-        // Switch back to exploring if empty
-        if (creep.store.getUsedCapacity() === 0) {
-            creep.memory.mode = config.MODE.exploring;
-            creep.memory.currentGoal = "findIntersection";
-            delete creep.memory.targetRoom;
-            return;
-        }
-        
         // Set target room to home if not set
-        if (!creep.memory.targetRoom) {
-            creep.memory.targetRoom = config.ROOM_NAME;
+        if (!creep.memory.homeRoom) {
+            creep.memory.homeRoom = config.ROOM_NAME;
         }
         
         // If not in home room, travel back
-        if (creep.room.name !== config.ROOM_NAME) {
-            const exitDir = creep.room.findExitTo(creep.memory.targetRoom);
-            const exit = creep.pos.findClosestByRange(exitDir);
-            creepHelper.moveTo(creep, exit);
+        if (creep.room.name !== creep.memory.homeRoom) {
+            this.travelToRoom(creep, creep.memory.homeRoom);
             return;
         }
         
         // In home room, find storage
-        const storage = creep.room.storage || creep.room.find(FIND_STRUCTURES, {
-            filter: s => s.structureType === STRUCTURE_STORAGE || s.structureType === STRUCTURE_CONTAINER
+        const storage = creep.room.find(FIND_STRUCTURES, {
+            filter: s => s.structureType === STRUCTURE_STORAGE || 
+                     s.structureType === STRUCTURE_CONTAINER
         })[0];
         
         if (storage) {
-            // Get first resource type in store
-            const resourceType = Object.keys(creep.store)[0];
-            
-            if (creep.transfer(storage, resourceType) === ERR_NOT_IN_RANGE) {
-                creepHelper.moveTo(creep, storage);
+            // Transfer all resources in inventory
+            for (const resourceType in creep.store) {
+                if (creep.transfer(storage, resourceType) === ERR_NOT_IN_RANGE) {
+                    creepHelper.moveTo(creep, storage);
+                    return;
+                }
             }
         } else {
             // No storage available, drop resources
-            const resourceType = Object.keys(creep.store)[0];
-            creep.drop(resourceType);
+            for (const resourceType in creep.store) {
+                creep.drop(resourceType);
+            }
         }
     }
 };
